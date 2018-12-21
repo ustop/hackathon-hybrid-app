8302dfbc10925950602a600e867aef09edf6fb1c
Ivan Wills <Ivan.Wills2@optus.com.au>
2018-12-21 12:09:09 +1100

    Trying to do navigation (but failing)

--- src/containers/Launch.js
+++ src/containers/Launch.js
@@ -1,5 +1,7 @@
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { compose, withState } from 'recompose';
+import { NavigationActions } from 'react-navigation';
 import moment from 'moment';
 
 import Launch from '../screens/Launch';
@@ -8,6 +10,9 @@ export default compose(
   connect(
     state => ({
 
+    }),
+    dispatch => ({
+      navigate: bindActionCreators(NavigationActions.navigate, dispatch),
     }),
   ),
   withState('isExtended', 'setIsExtended', false),
